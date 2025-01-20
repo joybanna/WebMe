2fdad28f6b173c05271a7ce9e421589c51f0dee1
joybanna <joybanna@gmail.com>
2025-01-20 22:04:55 +0700

    add file

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Route, Routes } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
+const root = document.getElementById('root')
+createRoot(root).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+    <Routes >
+    <Route path='/' element={<App />} />
+    <Route path='/about' element={<About />} />
+    <Route path='/portfolio' element={<Portfolio />} />
+    <Route path='/contact' element={<Contact />} />
+    </Routes >
+  </StrictMode>
 )
