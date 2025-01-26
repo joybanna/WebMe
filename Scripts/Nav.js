3b011b29f3b1b8c67e94a90d4bc6df8fd3c87533
joybanna <joybanna@gmail.com>
2025-01-26 23:03:02 +0700

    move files

--- Scripts/Nav.js
+++ Scripts/Nav.js
@@ -5,7 +5,7 @@ function ShowNav() {
         navBar.innerHTML = `
         <a class='w3-bar-item w3-button w3-right w3-padding-16 w3-hover-theme' href="../Pages/Contact.html"style="font-size: 20px;">Contact</a>
         <a class='w3-bar-item w3-button w3-right w3-padding-16 w3-hover-theme' href="../Pages/Protfolio.html"style="font-size: 20px;">Portfolio</a>
-        <a class='w3-bar-item w3-button w3-right w3-padding-16 w3-hover-theme' href="../Pages/Profile.html"style="font-size: 20px;">Home</a>
+        <a class='w3-bar-item w3-button w3-right w3-padding-16 w3-hover-theme' href="index.html"style="font-size: 20px;">Home</a>
        `;
     });
 }
