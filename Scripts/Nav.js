64fbd80d2af5757c05d2afe1bc511afc0560ab2c
joybanna <joybanna@gmail.com>
2025-01-27 09:47:33 +0700

    change name

--- Scripts/Nav.js
+++ Scripts/Nav.js
@@ -1,12 +1,14 @@
 function ShowNav() {
     document.addEventListener('DOMContentLoaded', () => {
         const navBar = document.getElementById('nav');
-        navBar.className = 'w3-top w3-bar w3-theme-d4';
-        navBar.innerHTML = `
-        <a class='w3-bar-item w3-button w3-right w3-padding-16 w3-hover-theme' href="Contact.html"style="font-size: 20px;">Contact</a>
-        <a class='w3-bar-item w3-button w3-right w3-padding-16 w3-hover-theme' href="Protfolio.html"style="font-size: 20px;">Portfolio</a>
+        var bar = document.createElement('div');
+        bar.className = 'w3-top w3-bar w3-theme-d4';
+        bar.innerHTML = `
+        <a class='w3-bar-item w3-button w3-right w3-padding-16 w3-hover-theme' href="02_Contact.html"style="font-size: 20px;">Contact</a>
+        <a class='w3-bar-item w3-button w3-right w3-padding-16 w3-hover-theme' href="01_Portfolio.html"style="font-size: 20px;">Portfolio</a>
         <a class='w3-bar-item w3-button w3-right w3-padding-16 w3-hover-theme' href="index.html"style="font-size: 20px;">Home</a>
        `;
+        navBar.appendChild(bar);
     });
 }
 ShowNav();
